4fc967ccc90d69abeaa443ff591e684c5fce6110
trupti@123v <truptik472@gmail.com>
2025-02-28 18:40:07 +0530

    Commit

--- WebAPIProject/Core/Models/DyeingMathod.cs
+++ WebAPIProject/Core/Models/DyeingMathod.cs
@@ -2,10 +2,7 @@
 {
     public class DyeingMathod
     {
-        public int ID { get; set; }
-
-        public string Name { get; set; }
-
+        public int ID { get; set; } 
         public string DyeMathods { get; set; }
 
     }
